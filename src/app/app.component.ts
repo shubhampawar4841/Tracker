8bd6c7b5c7779438121b4a2c23b8627f2b0f59b4
shubhampawar4841 <shubhampawar4036@gmail.com>
2025-01-31 21:07:23 +0530

    deplyment attempt 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { WorkoutFormComponent } from './workout-form/workout-form.component';
+import { WorkoutListComponent } from './workout-list/workout-list.component';
+import { WorkoutChartComponent } from './workout-chart/workout-chart.component';
+import {Chart,registerables} from 'chart.js'
 
+Chart.register(...registerables);
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [WorkoutFormComponent, WorkoutListComponent, WorkoutChartComponent], // ✅ Add WorkoutChartComponent
+  template: `
+<main>
+  <header class="brand-name text-6xl font-extrabold text-gray-800 text-center mt-8">
+    Workout-Tracker
+  </header>
+  <section class="content">
+    <app-workout-form></app-workout-form>
+    <app-workout-list></app-workout-list>
+    <app-workout-chart></app-workout-chart>  <!-- ✅ Add Chart Component -->
+  </section>
+</main>
+
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Tracker';
+  title = 'Workout Tracker';
 }
